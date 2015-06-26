ecaad2eec853f94406d6c58db6b555b8523fe979
Carlos <carlosjavierlopezlopez@gmail.com>
2015-06-26 22:59:25 +0200

    [PARTIDA]Ya se instancian los enemigos en el grid.

---
+++ BrainsEden/Assets/Scripts/ShooterDrone.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterDrone : MonoBehaviour {
+
+	public float vida;
+
+	void Awake()
+	{
+		vida= 10;
+	}
+
+	// Use this for initialization	
+	void Start () 
+	{
+		StartCoroutine(Disparar());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(vida <= 0f)
+			Destroy(this.gameObject);
+	}
+
+	IEnumerator Disparar()
+	{
+		while(true)
+		{
+			StartCoroutine(Rafaga());
+			yield return new WaitForSeconds(1f);
+		}
+	}
+	
+	IEnumerator Rafaga()
+	{
+		for(int i=0; i < 3; i++)
+		{
+			Debug.Log("BANG!");
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
