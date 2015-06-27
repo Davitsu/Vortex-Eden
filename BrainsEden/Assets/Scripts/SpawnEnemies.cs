7778f21b5f24abc6f58e2f3daf956019a5de0800
nimanym <nimanym@gmail.com>
2015-06-27 17:36:07 +0100

    Enemies! WE NEED ENEMIES!

---
+++ BrainsEden/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour {
+
+	public GameObject[] enemiesToSpawn;
+	public float delay= 6.0f;
+	float counter= 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter -= Time.deltaTime;
+		if (counter <= 0) {
+			counter=delay;
+			Instantiate(enemiesToSpawn[Random.Range(0,enemiesToSpawn.Length)]).transform.position=transform.position;
+		}
+	}
+}
