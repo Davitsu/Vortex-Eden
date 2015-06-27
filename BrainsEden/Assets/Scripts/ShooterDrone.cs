5e8be59e9b8ead2a28e68a3dd79360c70009f07b
Carlos <carlosjavierlopezlopez@gmail.com>
2015-06-27 08:46:58 +0200

    [ShooterDrone]Disparo corregido.

--- BrainsEden/Assets/Scripts/ShooterDrone.cs
+++ BrainsEden/Assets/Scripts/ShooterDrone.cs
@@ -3,41 +3,39 @@ using System.Collections;
 
 public class ShooterDrone : MonoBehaviour {
 
-	public float vida;
-
-	void Awake()
-	{
-		vida= 10;
-	}
-
-	// Use this for initialization	
-	void Start () 
-	{
-		StartCoroutine(Disparar());
-	}
+	public GameObject balaPrefab;
+	
+	public float vida = 10;
+	
+	public float disparaCada= 0.75f;
+	float contadorDisparo= 0f;
 	
 	// Update is called once per frame
 	void Update () 
-	{
+	{	
+		//control disparo
+		contadorDisparo+= Time.deltaTime;
+		
+		if(contadorDisparo >= disparaCada){
+			StartCoroutine(Rafaga ());
+			contadorDisparo= 0f;
+		}
+		
+		//control vida
 		if(vida <= 0f)
 			Destroy(this.gameObject);
-	}
-
-	IEnumerator Disparar()
-	{
-		while(true)
-		{
-			StartCoroutine(Rafaga());
-			yield return new WaitForSeconds(1f);
-		}
 	}
 	
 	IEnumerator Rafaga()
 	{
 		for(int i=0; i < 3; i++)
 		{
-			Debug.Log("BANG!");
-			yield return new WaitForSeconds(0.1f);
+			Instantiate(balaPrefab, this.gameObject.transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(0.15f);
 		}
+	}
+	
+	void Disparo(){
+		Instantiate(balaPrefab, this.gameObject.transform.position, Quaternion.identity);
 	}
 }
