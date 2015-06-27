8bb048edcb5fbd0a64f0a5aabbca6e0efbcfa99c
nimanym <nimanym@gmail.com>
2015-06-27 09:44:47 +0200

    Vida, balas

--- BrainsEden/Assets/Scripts/BulletMovement.cs
+++ BrainsEden/Assets/Scripts/BulletMovement.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class BulletMovement : MonoBehaviour {
 
-	public float speed=50.0f;
+	public float speed=80.0f;
 	public Vector2 direction;
+	public bool wiggle;
 	public float wiggleWidth=0.1f;
 	public float wiggleFreq=20.0f;
+	float timeCreated;
 
 	// Use this for initialization
 	void Start () {
+		timeCreated = Time.time;
 		direction = Vector2.right;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (direction * speed*Time.deltaTime);
-		direction.y = wiggleWidth*Mathf.Sin (Time.time*wiggleFreq)*wiggleFreq;
+		if (wiggle) {
+			direction.y = wiggleWidth * Mathf.Sin ((Time.time-timeCreated) * wiggleFreq) * wiggleFreq;
+		}
 	}
 }
---
+++ BrainsEden/Assets/Scripts/DieOffScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieOffScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height+100 || screenPosition.y < -100 || screenPosition.x > Screen.width+100 || screenPosition.x < -100)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
