f685863f97ea19df518a0671649a4d5c7b5c5407
Kevin <kevinfb92@gmail.com>
2015-06-27 14:44:24 +0100

    Store script

--- BrainsEden/Assets/Scripts/StoreScript.cs
+++ BrainsEden/Assets/Scripts/StoreScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class StoreScript : MonoBehaviour {
 
+	public int energy;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
