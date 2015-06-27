64919756c268db067e0bc97895a509539db698d5
David Giron <david.kitsu@gmail.com>
2015-06-27 17:39:12 +0200

    Arreglado

--- BrainsEden/Assets/Scripts/BuildingsColor.cs
+++ BrainsEden/Assets/Scripts/BuildingsColor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BuildingsColor : MonoBehaviour {
 	public Texture2D building1;
-	public Texture2D 
+	public Texture2D building2;
 
 	// Use this for initialization
 	void Start () {
