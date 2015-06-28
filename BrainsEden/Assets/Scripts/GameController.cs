d30f8244773ddcdf05a9500a660dbe9c1fc97ab6
Carlos <carlosjavierlopezlopez@gmail.com>
2015-06-28 13:35:49 +0200

    [GAME SCENE] gestión energía

--- BrainsEden/Assets/Scripts/GameController.cs
+++ BrainsEden/Assets/Scripts/GameController.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;		//para el label
 
 public class GameController : MonoBehaviour {
 
 	public int energia;
+	public Text labelEnergia;
 	
+	void Update()
+	{
+		//actualizacion de energia
+		labelEnergia.text= energia.ToString();
+	}
 }
